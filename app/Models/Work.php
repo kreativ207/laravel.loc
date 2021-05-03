84d58856a0a3c859e267b0a3186c075855bfeb6a
Nikolay <nikolaynigorodov@gmail.com>
2021-05-03 12:30:48 +0300

    Lesson 7 HomeWork Связь hasMany

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -5,12 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Student extends Model
+class Work extends Model
 {
     use HasFactory;
-
-    public function linkWork()
-    {
-        return $this->hasMany(Work::class, 'student_id', 'id');
-    }
 }
--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -4,22 +4,42 @@
     <div class="jumbotron">
         <h1 class="display-4">Students</h1>
     </div>
-    <ul class="list-group mt-4">
         @if(is_object($students) or is_array($students))
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Фамилия Имя</th>
+                    <th scope="col">Возраст</th>
+                    <th scope="col">Номер групы</th>
+                    <th scope="col">Номер курса</th>
+                    <th scope="col">Специальность</th>
+                    <th scope="col">Link ДЗ</th>
+                </tr>
+                </thead>
+
+            @php
+                $i = 1
+            @endphp
             @foreach($students as $student)
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div>
-                        {{$student->last_name}}
-                        <p style="font-size: 12px; color: #718096">{{$student->first_name}}</p>
-                    </div>
-                    <span class="badge badge-primary badge-pill">{{$student->age}}</span>
-                    <span class="badge badge-primary badge-pill">{{$student->group_number}}</span>
-                    <span class="badge badge-primary badge-pill">{{$student->course_number}}</span>
-                    <div>
-                        {{$student->specialty}}
-                    </div>
-                </li>
+                <tbody>
+                <tr>
+                    <th scope="row">{{$i}}</th>
+                    <td>{{$student->last_name}}, {{$student->first_name}}</td>
+                    <td>{{$student->age}}</td>
+                    <td>{{$student->group_number}}</td>
+                    <td>{{$student->course_number}}</td>
+                    <td>{{$student->specialty}}</td>
+                    <td>
+                        @foreach($student->linkWork as $link)
+                            {{$link->link}} ||
+                        @endforeach
+                    </td>
+                </tr>
+                @php
+                    $i += 1
+                @endphp
             @endforeach
+            </table>
         @endif
-    </ul>
 @endsection
